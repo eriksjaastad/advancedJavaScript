473d10fdc4531ec5a268b01a14bbeca8f065ee9c
Erik Sjaastad <spudlogic@gmail.com>
2015-05-11 18:34:19 -0700

    mostly done with lab1

--- node_intro/lab-backup.js
+++ node_intro/lab-backup.js
@@ -1,34 +1,30 @@
-"use strict";
 var request = require("request"),
-	cheerio = require("cheerio");
+	cheerio = require("cheerio"),
+	nodeInspector = require("node-inspector");
 
 var url = "https://en.wikipedia.org/wiki/May_8";
 
 request(url, function(err, responce, body) {
 	var $ = cheerio.load(body);
-	var title = [], count = [], names = [];
+	var title = [], count, names = [];
 	var json = {title : '', count : '', names: ''};
 
-	var getInfo = function(id, i) {
+	var getInfo = function(id) {
 		$('#' + id + '').filter(function(){
-			title[i] = $(this).text();
-
+			title = $(this).text();
 			$(this).parent().next().children().children().each(function(i, el){
 				names[i] = $(this).next('a:nth-child(2)').text();
 			});
 
-			count[i] = names.length;
-
 			json.title = title;
-			json.count = count;
+			json.count = names.length;
 			json.names = names.filter(Boolean);
 		});
 	};
 
 	var each = function(list, f) {
-		var i, item;
-		for (i = 0; i < list.length; i+=1) {
-			item = list[i];
+		for(var i = 0; i < list.length; i++) {
+			var item = list[i];
 			f(item, i);
 		}
 	};
